7da36abdea87e45da939f7d9da22241de927bb51
enzoefsilva <enzofurtuoso@gmail.com>
2024-08-14 22:45:41 -0400

    criando projeto fokus e tocando imagem e fundo

---
+++ script.js
@@ -0,0 +1,36 @@
+const html = document.querySelector('html');
+const displayTime = document.querySelector('#timer');
+const banner = document.querySelector('.app__image');
+const title = document.querySelector('.app__title');
+
+const botaoIniciar = document.querySelector(".app__card-primary-button");
+const focoBt = document.querySelector('.app__card-button--foco')
+
+const curtoBt = document.querySelector('.app__card-button--curto')
+
+const longoBt = document.querySelector('.app__card-button--longo')
+
+focoBt.addEventListener('click', () => {
+    alterarContexto('foco')
+})
+
+curtoBt.addEventListener('click', () => {
+    alterarContexto('descanso-curto')
+})
+
+longoBt.addEventListener('click', () => {
+   alterarContexto('descanso-longo')
+})
+
+function alterarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `./imagens/${contexto}.png`)
+}
+
+
+const duracaoFoco = 1500;
+const duracaoDescansoCurto = 300;
+const duracaoDescansoLongo = 900;
+
+
+
